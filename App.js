95ef61bb2e0d3ad1ff0db60583e4825ce40408f6
Thiago Dallacqua <thiagodallacqua@outlook.com>
2018-02-15 22:44:31 -0300

    finished chapter 2

--- App.js
+++ App.js
@@ -1,48 +1,18 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-
-import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={styles.container}>
+        <View>
           <AddEntry />
         </View>
       </Provider>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btn: {
-    backgroundColor: '#E53224',
-    padding: 10,
-    paddingLeft: 50,
-    paddingRight: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  btnText: {
-    color: '#fff'
-  }
-});
